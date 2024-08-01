bc4266f9be3a3de79601047bcb2419ff39ea1ec4
Arzoo Singh <singharzoo396@gmail.com>
2024-08-01 15:47:53 +0530

    tailwind css changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    distDir: 'build',
+  };
+  
+  export default nextConfig;
+  
